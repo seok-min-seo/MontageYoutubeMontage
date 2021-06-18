9ba44828737d5aa123aa20463610458a9fb1b290
seok-min-seo <51194291+seok-min-seo@users.noreply.github.com>
2021-06-18 13:32:55 +0900

    20210618 초기화
    초기화

--- MontageYoutube2t/app/src/main/java/com/sukddaksoftware/montageyoutube2t/Fragments/HomeFragment.kt
+++ MontageYoutube2t/app/src/main/java/com/sukddaksoftware/montageyoutube2t/Fragments/HomeFragment.kt
@@ -9,9 +9,7 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.androidnetworking.AndroidNetworking
-import com.androidnetworking.error.ANError
-import com.androidnetworking.interfaces.ParsedRequestListener
+
 import com.sukddaksoftware.montageyoutube2t.viewModel.MainViewModel
 import com.sukddaksoftware.montageyoutube2t.Adapter.YoutubeAdapter
 import com.sukddaksoftware.montageyoutube2t.Models.Item
@@ -33,39 +31,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         savedInstanceState: Bundle?
     ): View? {
         val youtubeAdapter = YoutubeAdapter(dataList)
-        AndroidNetworking.initialize(context)
-        AndroidNetworking.get("https://www.googleapis.com/youtube/v3")
-                .build()
-                .getAsObject(Youtube::class.java, object : ParsedRequestListener<Youtube>{
-                    override fun onResponse(response: Youtube) {
-                        dataList.addAll(response.items)
-                        youtubeAdapter.notifyDataSetChanged()
-                    }
-
-                    override fun onError(anError: ANError?) {
-                        TODO("Not yet implemented")
-                    }
 
 
 
-                })
-
-        recyclerView.apply {
-            recyclerView.layoutManager = LinearLayoutManager(context,
-                RecyclerView.VERTICAL,
-                false
-            )
-            adapter = youtubeAdapter
-        }
-        viewModel.apply {
-            itemLiveData.observe(viewLifecycleOwner, Observer {
-
-            })
-
-            loadingLiveData.observe(viewLifecycleOwner, Observer { isLoading ->
-               // progressBar2.visibility = if (isLoading) View.VISIBLE else View.GONE
-            })
-        }
 
 
         return super.onCreateView(inflater, container, savedInstanceState)
